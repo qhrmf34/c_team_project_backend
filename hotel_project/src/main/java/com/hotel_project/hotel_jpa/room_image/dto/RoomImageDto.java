9ac4c6e03799da50e1969e4a45bada34fd3bf671
soungminkim <21soungmin12@naver.com>
2025-09-10 18:56:37 +0900

    roomimage 완료

---
+++ hotel_project/src/main/java/com/hotel_project/hotel_jpa/room_image/dto/RoomImageDto.java
@@ -0,0 +1,30 @@
+package com.hotel_project.hotel_jpa.room_image.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoomImageDto implements IRoomImage {
+    private Long id;
+
+    private Long roomId;
+
+    @NotBlank(message = "객실 이미지 이름은 필수 입력 입니다.")
+    @Size(max = 255, message = "객실 이미지 이름은 255자 이하로 입력해야 합니다.")
+    private String roomImageName;
+
+    @NotBlank(message = "객실 이미지 경로는 필수 입력 입니다.")
+    @Size(max = 500, message = "객실 이미지 경로는 500자 이하로 입력해야 합니다.")
+    private String roomImagePath;
+
+    private Long roomImageSize;
+
+    private LocalDateTime createdAt;
+}
---
+++ hotel_project/src/main/java/com/hotel_project/hotel_jpa/room_image/dto/RoomImageEntity.java
@@ -0,0 +1,61 @@
+package com.hotel_project.hotel_jpa.room_image.dto;
+
+import com.hotel_project.hotel_jpa.room.dto.RoomEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table (name = "room_image_tbl")
+public class RoomImageEntity implements IRoomImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private RoomEntity room;
+
+    @Column(nullable = false, length = 255)
+    private String roomImageName;
+
+    @Column(nullable = false, length = 500)
+    private String roomImagePath;
+
+    private Long roomImageSize;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        if (createdAt == null) {
+            createdAt = now;
+        }
+    }
+
+    @Override
+    public Long getRoomId() {
+        if (this.room == null) {
+            return 0L;
+        }
+        return this.room.getId();
+    }
+
+    @Override
+    public void setRoomId(Long roomId) {
+        if (this.room == null) {
+            return;
+        }
+        this.room.setId(roomId);
+    }
+}
