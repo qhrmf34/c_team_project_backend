d1255aa158eccfa7c4a119f3a8daa8a01cf40838
nol1214 <merylqls169@gmail.com>
2025-09-11 19:14:37 +0900

    수정1

---
+++ hotel_project/src/main/java/com/hotel_project/member_jpa/member/dto/MemberEntity.java
@@ -0,0 +1,64 @@
+package com.hotel_project_backend.member_jpa.member.dto;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "member_tbl")
+public class MemberEntity implements IMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column( nullable = false, unique = true, length = 30)
+    private String phoneNumber;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Provider provider = Provider.local;
+
+    @Column(nullable = true, unique = true, length = 255)
+    private String providerId;
+
+    @Column(nullable = false, updatable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(nullable = true,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        if (createdAt == null) {
+            createdAt = now;
+        }
+        if (updatedAt == null) {
+            updatedAt = now;
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ hotel_project/src/main/java/com/hotel_project/member_jpa/member/dto/Provider.java
@@ -0,0 +1,5 @@
+package hotel_project.src.main.java.com.hotel_project.member_jpa.member.dto;
+
+public enum Provider {
+    local, google, facebook, apple
+}
