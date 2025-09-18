67886fa3f4f6b73f31bde712a04f26857bb1bc96
soungminkim <21soungmin12@naver.com>
2025-09-18 14:36:27 +0900

    Feat: 추가 MemberImageDto, MemberImageEntity, IMemberImage, ImageType
    - MemberImageDto: 사용자 이미지 테이블 DTO 클래스 구현
    
    - MemberImageEntitys: 사용자 이미지 테이블 JPA 엔티티 구현
    
    - IMemberImage: 사용자 이미지 테이블 인터페이스 정의 DTO - Entity 변환 지원
    
    - ImageType: 사용자 이미지 타입을 지정한 Enum

---
+++ hotel_project/src/main/java/com/hotel_project/member_jpa/member_image/dto/MemberImageEntity.java
@@ -0,0 +1,84 @@
+package com.hotel_project.member_jpa.member_image.dto;
+
+import com.hotel_project.common_jpa.dto.IId;
+import com.hotel_project.member_jpa.member.dto.MemberEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class MemberImageEntity implements IMemberImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "member_id", nullable = false)
+    private MemberEntity memberEntity;
+
+    @Transient
+    private Long memberId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ImageType imageType;
+
+    @Column(nullable = false,length = 255)
+    private String memberImageName;
+
+    @Column(nullable = false, length = 500)
+    private String memberImagePath;
+
+    private Long memberImageSize;
+
+    @Column(nullable = false, updatable = false,
+            columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Override
+    public IId getMember(){
+        return this.memberEntity;
+    }
+
+    @Override
+    public void setMember(IId iId) {
+        if (iId == null){
+            return;
+        }
+        if (this.memberEntity == null){
+            this.memberEntity = new MemberEntity();
+        }
+        this.memberEntity.copyMembersId(iId);
+    }
+
+    @Override
+    public Long getMemberId() {
+        return this.memberEntity != null ? this.memberEntity.getId() : null;
+    }
+
+    @Override
+    public void setMemberId(Long memberId) {
+        if (memberId == null){
+            throw new IllegalArgumentException("memberId cannot be null");
+        }
+        if (this.memberEntity == null){
+            this.memberEntity = new MemberEntity();
+        }
+        this.memberEntity.setId(memberId);
+        this.memberId = memberId;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        if (createdAt == null) {
+            createdAt = now;
+        }
+    }
+}
